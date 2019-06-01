420279bb4d5f1207c1d5e2975eb92e470034195b
AminTalebi <talebi242@gmail.com>
2019-06-01 18:15:17 +0430

    Profile Model

---
+++ microtask/profile_manager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProfileManagerConfig(AppConfig):
+    name = 'profile_manager'
---
+++ microtask/profile_manager/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    NORMAL_USER = 'NO'
+    REQUESTER = 'RQ'
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_num = models.CharField(max_length=30)
+    city = models.CharField(max_length=250)
+    country = models.CharField(max_length=250)
+    gender = models.CharField(max_length=10)
+    avatar = models.ImageField()
+
+    def __str__(self):
+        return self.user.username 
+        
