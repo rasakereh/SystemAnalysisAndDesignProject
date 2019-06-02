20db966fa4e0b9812940cb303dcc05689247be89
AminTalebi <talebi242@gmail.com>
2019-06-02 13:33:24 +0430

    Login init

--- microtask/profile_manager/models.py
+++ microtask/profile_manager/models.py
@@ -2,10 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    NORMAL_USER = 'NO'
-    REQUESTER = 'RQ'
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_num = models.CharField(max_length=30)
+    phone_num = models.CharField(max_length=30, unique=True)
     city = models.CharField(max_length=250)
     country = models.CharField(max_length=250)
     gender = models.CharField(max_length=10)
--- microtask/profile_manager/views.py
+++ microtask/profile_manager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from profile_manager.models import Profile
+
 import json
 
 @csrf_exempt
@@ -8,6 +11,41 @@ def register(req):
     if req.method == 'POST':
         body = dict(req.POST)
         body = json.loads(list(body.keys())[0])
-        print(body)
+        username = body['username']
+        email = body['email']
+        nationalid = body['nationalid']
+        
+        user = User.objects.filter(username=body['username'])
+        if len(user) == 1:
+            return HttpResponse("username", content_type="text/plain")
 
-    return HttpResponse("SUCCESS", content_type="text/plain")
+        user = User.objects.filter(email=body['email'])
+        if len(user) == 1:
+            return HttpResponse("email", content_type="text/plain")
+        
+        user = Profile.objects.filter(phone_num=body['phoneNum'])
+        if len(user) == 1:
+            return HttpResponse("phone number", content_type="text/plain")
+        
+
+        user = User(username=username,
+                        password=body['password'],
+                        email=body['email'],
+                        first_name=body['firstName'],
+                        last_name=body['lastName'])
+        user.save()
+        profile = Profile(user=user,
+                phone_num=body['phoneNum'],
+                city=body['city'],
+                country=body['country'],
+                gender=body['gender'])
+
+        profile.save()
+        return HttpResponse("SUCCESS", content_type="text/plain")
+
+    return HttpResponse("NOT A FORM", content_type="text/plain")
+    
+def login(req):
+    if req.method == 'POST':
+        body = dict(req.POST)
+        body = json.loads(list(body.keys())[0])
